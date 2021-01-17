e861d37809581b6c8e5541232a6ecd03e08d2a67
byr2013176 <byr2013176@maricopa.edu>
2021-01-17 09:16:58 -0700

    fixed contact page background

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-const navbar = document.getElementById('navbar');
+// const navbar = document.getElementById('navbar');
 
-window.onscroll = function() {
-  window.pageYOffset > 600 ? navbar.classList.remove('top') : navbar.classList.add('top');
-}
+// window.onscroll = function() {
+//   window.pageYOffset > 600 ? navbar.classList.remove('top') : navbar.classList.add('top');
+// }
+
+$(window).scroll(function() {
+  if($(this).scrollTop() > 300) {
+      $('.navbar').addClass('top');
+  } else {
+      $('.navbar').removeClass('top');
+  }
+});
