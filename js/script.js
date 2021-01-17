dae5afcaaf81a452c17ff01863e706d4ec1672ea
byr2013176 <byr2013176@maricopa.edu>
2021-01-17 09:24:44 -0700

    fixed navbar transparency

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-// const navbar = document.getElementById('navbar');
+const navbar = document.getElementById('navbar');
 
-// window.onscroll = function() {
-//   window.pageYOffset > 600 ? navbar.classList.remove('top') : navbar.classList.add('top');
-// }
-
-$(window).scroll(function() {
-  if($(this).scrollTop() > 300) {
-      $('.navbar').addClass('top');
-  } else {
-      $('.navbar').removeClass('top');
-  }
-});
+window.onscroll = function() {
+  window.pageYOffset > 600 ? navbar.classList.remove('top') : navbar.classList.add('top');
+}
