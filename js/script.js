669e57a2b63bf434b16922e85bdfec5b8841b72e
byronmosley <mosleyb91@outlook.com>
2021-01-18 13:19:45 -0700

    allow animation for video on movile screens as well as desktop, create hamburger menu for mobile

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ const navbar = document.getElementById('navbar');
 window.onscroll = function() {
   window.pageYOffset > 600 ? navbar.classList.remove('top') : navbar.classList.add('top');
 }
+
+const menuToggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  menu.classList.toggle('active');
+})
+
+function removeActive() {
+  menuToggle.classList.remove('active');
+  menu.classList.remove('active');
+}
