d2d1cd53cac3c9f4d1c6fdd2725555cd56faad42
byronmosley <mosleyb91@outlook.com>
2021-03-27 16:14:21 -0700

    Migrated projects to modals, rewrote site to implement Bootstrap technology, replaced glitch animation to match background theme, changed color scheme to dark mode

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
-const navbar = document.getElementById('navbar');
+// Fade graphic and nav background
 
-window.onscroll = function() {
-  window.pageYOffset > 600 ? navbar.classList.remove('top') : navbar.classList.add('top');
-}
+const checkpoint = 500
 
-const menuToggle = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
-
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  menu.classList.toggle('active');
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint
+    navOpacity = 0 + currentScroll / checkpoint
+  } else {
+    opacity = 0
+    navOpacity = 1
+  }
+  document.querySelector('.glitch').style.opacity = opacity
+  document.querySelector('.overlay').style.opacity = navOpacity
 })
 
-function removeActive() {
-  menuToggle.classList.remove('active');
-  menu.classList.remove('active');
-}
+// Toggle menu button from hamburger to 'x'
+
+const menuToggle = document.querySelector('.toggle')
+const menu = document.querySelector('.menu')
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active')
+  menu.classList.toggle('active')
+})
+
+// Close Hamburger Menu on section click
+
+const selectedSections = document.querySelectorAll('.nav-item')
+
+selectedSections.forEach((section) => {
+  section.addEventListener('click', () => {
+    menuToggle.classList.remove('active')
+    menu.classList.remove('active')
+  })
+})
